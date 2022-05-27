1c045186fb2476c9050853105e749e07b323eef5
Diego Huamani <dhuamanilu@unsa.edu.pe>
2022-05-27 15:59:45 -0500

    Ejercicio 2f resuelto

--- Ejercicio2f.py
+++ Ejercicio2f.py
@@ -1,2 +1,17 @@
 from interpreter import draw
 from chessPictures import *
+dominoArriba=square.join(square.negative())
+dominoAbajo=square.negative().join(square)
+cuadrado=dominoAbajo.up(dominoArriba)
+rpta=cuadrado
+for i in range (3):
+	rpta=rpta.join(cuadrado)
+
+rpta=rpta.up(rpta)
+draw(rpta)
+
+
+
+
+
+
